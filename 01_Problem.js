7ddb8ff1604eb41650cb2df5c229c09a02b8113e
Sushant Sharma <sushant.x01@gmail.com>
2022-06-29 09:07:54 +0530

    First Problem Solved

---
+++ 01_Problem.js
@@ -0,0 +1,46 @@
+/* PROBLEM STATEMENT:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+} */
+
+const listOfProducts = [
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television",
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner",
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television",
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner",
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan",
+  },
+];
+
+const getUniqueProductCount = listOfProducts.reduce((ac, cu) => {
+  ac[cu.productName] = (ac[cu.productName] || 0) + 1;
+  return ac;
+}, {});
+
+console.log(getUniqueProductCount);
